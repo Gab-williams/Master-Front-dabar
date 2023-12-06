51299222b28aad27eb1db254a9036e4d46d92cdb
Gab-williams <81557989+Gab-williams@users.noreply.github.com>
2023-12-06 16:30:33 +0100

    update test

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -158,7 +158,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "87e300da562a671f"; }
+/******/ 		__webpack_require__.h = function() { return "71a7ac7c01d061fa"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/_app.js
+++ pages/_app.js
@@ -13,6 +13,7 @@ import '../public/assets/css/main.css'
 import '../public/assets/css/slick.css'
 import '../public/assets/css/spacing.css'
 import '../public/assets/css/swiper-bundle.css'
+
 function MyApp({ Component, pageProps }) {
 
     const [loading, setLoading] = useState(true)
