f09c2da7acd178e50740f5793c8a18eef0ed3669
Gabriel williams <81557989+Gab-williams@users.noreply.github.com>
2023-12-11 14:52:38 +0100

    blog date

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -59,7 +59,7 @@ export default function BlogDetails() {
         }
       } catch (error) {}
 
-      let timez = new Date(story.sys.createdAt);
+      let timez = new Date(story.sys.updatedAt);
       const monthNames = [
         "Jan",
         "Feb",
@@ -211,7 +211,7 @@ export default function BlogDetails() {
                         <li>
                           <Link href="/blog">{dataLocial?.writer}.</Link>
                         </li>
-                        <li>{datex}</li>
+                        <li>{dataLocial.timez}</li>
                         <li>{readtime}</li>
                       </ul>
                       <h2 className="title">{fieldsdata.heading}</h2>
